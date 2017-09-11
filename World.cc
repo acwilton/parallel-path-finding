9a3dc9607f96dc21a9f1dee1240972db38457459
Alex Wilton <student.alexwilton@gmail.com>
2017-09-11 00:18:12 -0400

    Added operator>> overloading for World class. This will make it very easy for the gui to pick up data from a file for display.

--- World.cc
+++ World.cc
@@ -4,28 +4,39 @@
 
 #include <random>
 #include <functional>
+#include <cmath>
+#include <iostream>
 
 typedef unsigned char uchar;
 
 namespace parPath
 {
+
+World::World ()
+        : m_height (0),
+          m_width (0)
+{
+}
+
 World::World (size_t height, size_t width)
         : m_height (height),
-          m_width (width),
-          m_tiles (height, std::vector<tile_t> (width,
-          { static_cast<uchar> (0) }))
+          m_width (width)
 {
-    generateMap ();
 }
 
 void World::generateMap ()
 {
+    m_tiles = std::vector<std::vector<tile_t>>(m_height,
+            std::vector<tile_t> (m_width, { static_cast<uchar> (0) }));
+
     std::random_device rd;
     std::minstd_rand0 gen (rd ());
 
     // Starting position in map
-    size_t x = gen () % m_width;
-    size_t y = gen () % m_height;
+    uint xMiddle = ceil (static_cast<float>(m_width) / 2);
+    uint yMiddle = ceil (static_cast<float>(m_height) / 2);
+    size_t x = gen () % xMiddle + (xMiddle / 2);
+    size_t y = gen () % yMiddle + (yMiddle / 2);
     m_tiles[y][x].cost = static_cast<uchar> (1);
 
     size_t numCarvedTiles = 1;
@@ -74,6 +85,16 @@ void World::generateMap ()
 
 }
 
+size_t World::getWidth () const
+{
+    return m_width;
+}
+
+size_t World::getHeight () const
+{
+    return m_height;
+}
+
 World::tile_t World::operator() (size_t row, size_t column)
 {
     return m_tiles[row][column];
@@ -81,7 +102,7 @@ World::tile_t World::operator() (size_t row, size_t column)
 
 std::ostream& operator<< (std::ostream& stream, const World& world)
 {
-    stream << world.m_height << world.m_width;
+    stream << world.m_height << "\n" << world.m_width << "\n";
     for (auto& row : world.m_tiles)
     {
         for (auto& t : row)
@@ -90,20 +111,21 @@ std::ostream& operator<< (std::ostream& stream, const World& world)
             stream << static_cast<uchar> (t.cost);
         }
     }
+    stream << "\n";
     return stream;
 }
 
 std::istream& operator>> (std::istream& stream, World& world)
 {
-    size_t height, width;
-    stream >> height >> width;
-    std::vector<std::vector<World::tile_t>> tiles (height, std::vector(width));
+    stream >> world.m_height >> world.m_width;
+    world.m_tiles.resize(world.m_height);
 
-    for (auto& row : tiles)
+    for (auto& row : world.m_tiles)
     {
-        for (auto& t : row)
+        row.resize (world.m_width);
+        for (auto& tile : row)
         {
-            stream >> t.cost;
+            stream >> tile.cost;
         }
     }
 

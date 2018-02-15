e69dae9619c5707536e37c605924cbf01d054091
Alex Wilton <student.alexwilton@gmail.com>
2018-02-14 21:16:52 -0500

    Finished path gen.

--- src/worldGen/PathGen.cc
+++ src/worldGen/PathGen.cc
@@ -36,72 +36,189 @@ int main (int args, char* argv[])
 
     uint maxThreshold = world.getWidth() + world.getHeight ();
     // Find Start Point
-    pathFind::Point start;
-    start.x = 0;
-    start.y = 0;
+    pathFind::Point topLeft;
+    topLeft.x = 0;
+    topLeft.y = 0;
     for (uint i = 1; i < maxThreshold; ++i)
     {
-        bool foundStart = false;
-        pathFind::Point temp = start;
+        bool foundTopLeft = false;
+        pathFind::Point temp = topLeft;
         while(temp.x < world.getWidth () && temp.y < world.getHeight())
         {
             if (world(temp.x, temp.y).cost != 0)
             {
-                foundStart = true;
+                foundTopLeft = true;
                 break;
             }
             --temp.x;
             ++temp.y;
         }
 
-        if (foundStart)
+        if (foundTopLeft)
         {
-            start = temp;
+            topLeft = temp;
             break;
         }
 
         if (i < world.getWidth())
         {
-            ++start.x;
+            ++topLeft.x;
         }
         else
         {
-            ++start.y;
+            ++topLeft.y;
         }
     }
 
-    pathFind::Point end;
-    end.x = world.getWidth() - 1;
-    end.y = world.getHeight() - 1;
+    pathFind::Point topRight;
+    topRight.x = world.getWidth() - 1;
+    topRight.y = 0;
     for (uint i = 1; i < maxThreshold; ++i)
     {
-        bool foundEnd = false;
-        pathFind::Point temp = end;
+        bool foundTopRight = false;
+        pathFind::Point temp = topRight;
         while(temp.x < world.getWidth () && temp.y < world.getHeight())
         {
             if (world(temp.x, temp.y).cost != 0)
             {
-                foundEnd = true;
+                foundTopRight = true;
+                break;
+            }
+            ++temp.x;
+            ++temp.y;
+        }
+
+        if (foundTopRight)
+        {
+            topRight = temp;
+            break;
+        }
+
+        if (i < world.getWidth())
+        {
+            --topRight.x;
+        }
+        else
+        {
+            ++topRight.y;
+        }
+    }
+
+    pathFind::Point botLeft;
+    botLeft.x = 0;
+    botLeft.y = world.getHeight() - 1;
+    for (uint i = 1; i < maxThreshold; ++i)
+    {
+        bool foundBotLeft = false;
+        pathFind::Point temp = botLeft;
+        while(temp.x < world.getWidth () && temp.y < world.getHeight())
+        {
+            if (world(temp.x, temp.y).cost != 0)
+            {
+                foundBotLeft = true;
+                break;
+            }
+            ++temp.x;
+            ++temp.y;
+        }
+
+        if (foundBotLeft)
+        {
+            botLeft = temp;
+            break;
+        }
+
+        if (i < world.getHeight())
+        {
+            --botLeft.y;
+        }
+        else
+        {
+            ++botLeft.x;
+        }
+    }
+
+    pathFind::Point botRight;
+    botRight.x = world.getWidth() - 1;
+    botRight.y = world.getHeight() - 1;
+    for (uint i = 1; i < maxThreshold; ++i)
+    {
+        bool foundBotRight = false;
+        pathFind::Point temp = botRight;
+        while(temp.x < world.getWidth () && temp.y < world.getHeight())
+        {
+            if (world(temp.x, temp.y).cost != 0)
+            {
+                foundBotRight = true;
                 break;
             }
             --temp.x;
             ++temp.y;
         }
 
-        if (foundEnd)
+        if (foundBotRight)
         {
-            end = temp;
+            botRight = temp;
             break;
         }
 
         if (i < world.getHeight())
         {
-            --end.y;
+            --botRight.y;
         }
         else
         {
-            --end.x;
+            --botRight.x;
         }
+    }
+
+    pathFind::Point start, end;
+    start = topLeft;
+    end = botRight;
+    uint maxDist = abs (static_cast<int> (start.x) - static_cast<int> (end.x))
+        + abs (static_cast<int> (start.y) - static_cast<int> (end.y));
+
+    uint trtl = abs (static_cast<int> (topRight.x) - static_cast<int> (topLeft.x))
+        + abs (static_cast<int> (topRight.y) - static_cast<int> (topLeft.y));
+    if (trtl > maxDist)
+    {
+        maxDist = trtl;
+        end = topRight;
+    }
+
+    uint bltl = abs (static_cast<int> (botLeft.x) - static_cast<int> (topLeft.x))
+        + abs (static_cast<int> (botLeft.y) - static_cast<int> (topLeft.y));
+    if (bltl > maxDist)
+    {
+        maxDist = bltl;
+        end = botLeft;
+    }
+
+    uint brtr = abs (static_cast<int> (botRight.x) - static_cast<int> (topRight.x))
+        + abs (static_cast<int> (botRight.y) - static_cast<int> (topRight.y));
+    if (brtr > maxDist)
+    {
+        maxDist = brtr;
+        start = botRight;
+        end = topRight;
+    }
+
+    uint brbl = abs (static_cast<int> (botRight.x) - static_cast<int> (botLeft.x))
+        + abs (static_cast<int> (botRight.y) - static_cast<int> (botLeft.y));
+    if (brbl > maxDist)
+    {
+        maxDist = brbl;
+        start = botRight;
+        end = botLeft;
+    }
+
+    uint bltr = abs (static_cast<int> (botLeft.x) - static_cast<int> (topRight.x))
+        + abs (static_cast<int> (botLeft.y) - static_cast<int> (topRight.y));
+    if (bltr > maxDist)
+    {
+        maxDist = bltr;
+        start = botLeft;
+        end = topRight;
     }
 
     std::stringstream pathFileName;
